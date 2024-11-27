4df896a3845dd0e9615c3cf2fc76ebd7a28947f9
Diana <dianabasheguroval7@mail.ru>
2024-11-27 16:22:09 +0500

    Создан каркас

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <array>
+#include <fstream>
+
+using namespace std;
+vector<vector<string >> arr;
+ifstream in("la.txt");
+
+void f1(){
+}
+void f2(){
+}
+void f3(){
+}
+
+int main()
+{
+    void f1();
+    void f2();
+    void f3();
+    return 0;
+}
