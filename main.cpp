52ee93df94448e726afd045b037f8e84a3ddbc88
Diana <dianabasheguroval7@mail.ru>
2024-11-27 16:53:34 +0500

    Создана функция 1 и добавлен текстовый файл

--- main.cpp
+++ main.cpp
@@ -6,20 +6,32 @@
 #include <fstream>
 
 using namespace std;
-vector<vector<string >> arr;
+
+vector<vector<string>> arr;
 ifstream in("la.txt");
 
 void f1(){
+    string line;
+    if (in.is_open()){
+        while (getline(in, line)) {
+                arr.push_back({{line[0]}, line.substr(2)}); // Добавляем в массив: 1 символ строки - "Ключ", 3 символ - "Значение"
+        }
+        in.close(); // Закрываем файл после чтения
+    } else {
+        cout << "Ошибка открытия файла." << endl;
+    }
 }
+
 void f2(){
 }
+
 void f3(){
 }
 
 int main()
 {
-    void f1();
-    void f2();
-    void f3();
+    f1();
+    f2();
+    f3();
     return 0;
 }
